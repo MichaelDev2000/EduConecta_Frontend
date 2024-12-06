8d846c9fa221e35233f84135f431c0ef82e0d449
kevinospina142 <kh5564765@gmail.com>
2024-12-06 16:56:01 -0500

    creacion editar perfil

--- src/app/v-educonecta/cperfil/cperfil.component.ts
+++ src/app/v-educonecta/cperfil/cperfil.component.ts
@@ -15,14 +15,6 @@ export class CperfilComponent {
         biografia:`Ubicación:Bogotá,Colombia \n\ncorreo:pepitoperez604@gmail.com \nhola buenas tardes`,
       }
       
-      // Puedes agregar más amigos aquí
     ];
     
-  
-    // Función para abrir el chat con un amigo
-    abrirChat(amigo: any) {
-      console.log('Abriendo chat con:', amigo.nombre);
-      // Aquí puedes redirigir a otra página de chat o abrir un modal de chat
-      // Por ejemplo: this.router.navigate(['/chat', amigo.id]);
-    }
 }
